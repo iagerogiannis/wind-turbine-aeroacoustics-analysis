5866a449fcf6cdaf1cc1f01547b0080bb11f6590
Ioannis Gerogiannis <iagerogiannis@gmail.com>
2022-01-16 17:54:37 +0200

    Temp SPL field calculation

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ from lib.output import *
 results_dir = '../results'
 
 # Fasma hxhtikhs isxyos
-f = [16., 31.5, 63., 125., 250., 500., 1000., 2000., 4000., 8000.][0:5]
+f = [16., 31.5, 63., 125., 250., 500., 1000., 2000., 4000., 8000.][0:4]
 Lw = [85., 90., 95., 103., 110., 110., 105., 99.5, 97.5, 84.5]
 
-order = 1
+order = 2
 
 print_title()
-solve(f, order, results_dir, delete_old=False)
+solve(f, order, results_dir, delete_old=True)
 plot(f, results_dir)
